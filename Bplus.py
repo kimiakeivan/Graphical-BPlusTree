7175b5fd271bcd5bf8baba92528850962fd37118
kimiakeivan <velizalester@gmail.com>
2025-01-06 22:33:10 +0330

    add search

--- Bplus.py
+++ Bplus.py
@@ -1,17 +1,20 @@
-from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsEllipseItem, QGraphicsTextItem
-from PyQt5.QtGui import QBrush, QPen
-from PyQt5.QtCore import Qt
-import sys
-
-
 class Node:
     def __init__(self, is_leaf=False):
-        self.is_leaf = is_leaf     
-        self.keys = []             
-        self.children = []          
-        self.next = None 
-        self.parent = None          
+        self.is_leaf = is_leaf      
+        self.keys = []              
+        self.children = [] 
+        # Deleted next and parent cause it's already being supported by Node class keys and children
 
+
+    # Use this for debugging
+    def get_dict(self):
+        if self.is_leaf:
+            return {
+                "keys": self.keys,
+                "children": [],
+                "is_leaf": True
+                }
+        return {"keys": self.keys, "children": [child.get_dict() for child in self.children], "is_leaf": False}
 
 
 class BPlusTree:
@@ -20,7 +23,7 @@ class BPlusTree:
         self.max_degree = max_degree
 
 
-    
+    # TODO: add function for inserting into parent and leafs as well
     def insert(self, key):
         leaf = self.find_leaf(self.root, key)
         leaf.keys.append(key)
@@ -33,13 +36,11 @@ class BPlusTree:
     def find_leaf(self, node, key):
         if node.is_leaf:
             return node
+
         for i, item in enumerate(node.keys):
             if key < item:
-                return self.find_leaf(node.children[i], key)   
-                         
+                return self.find_leaf(node.children[i], key)
         return self.find_leaf(node.children[-1], key)
-
-
 
 
     def split(self, node):
@@ -50,34 +51,24 @@ class BPlusTree:
         node.keys = node.keys[:mid_index]
 
         if not node.is_leaf:
-            del new_node.keys[0]
-            # print(new_node.keys)
-            new_node.children =node.children[mid_index+1:]
-            node.children = node.children[:mid_index+1]
-            # for i in new_node.children:
-            #     print(i.keys)
+            new_node.children =node.children[mid_index:]
+            node.children = node.children[:mid_index]
 
-        
-
-        if node==self.root:
+        if node == self.root:
             new_root = Node()
             new_root.keys = [mid_key]
             new_root.children = [node, new_node]
             self.root = new_root
+
         else:
             parent = self.find_parent(self.root, node)
             parent.keys.append(mid_key)
             parent.keys.sort()
             parent.children.insert(parent.keys.index(mid_key)+1, new_node)
             if len(parent.keys) >= self.max_degree:
-                # print("before ",parent.keys)
                 self.split(parent)
-                # print("after ",parent.keys)
 
 
-
-
-    
     def find_parent(self, node, child):
         if node.is_leaf:
             return None
@@ -92,117 +83,48 @@ class BPlusTree:
         return None
 
 
-
-
     def search(self, searchkey):
         node = self.root
         while not node.is_leaf:
-            for i,key in enumerate(node.keys):
+            for i, key in enumerate(node.keys):
                 if searchkey < key:
-                        node = node.children[i]
-                        break
+                    node = node.children[i]
+                    break
             else:
-                    node = node.children[-1]     
-
+                node = node.children[-1]
 
         for i, key in enumerate(node.keys):
             if key == searchkey:
                 return True
-        return False    
+        return False
 
 
 
-
+    # TODO: add function to merge nodes after deletion
     def delete(self):
         pass
 
-    def merge(self):
-        pass
 
 
+    def get_dict(self):
+        if self.root == None:
+            return {'root': {}}
+        return {'root': self.root.get_dict()}
 
 
+# Add this if __name__ == "__main__" in case you wanted to import this file for graphical view
+if __name__ == "__main__":
+    tree = BPlusTree(max_degree=4)
+    for key in [10, 20, 5, 15, 25, 11, 13, 40, 4, 22, 36, 17]:
+        tree.insert(key)
+    print(tree.get_dict())
+    print()
+    print("Root keys:", tree.root.keys)
+    for i, child in enumerate(tree.root.children):
+        print(f"child{i} keys: {child.keys}")
+        for j, childen in enumerate(child.children):
+            print(f"child{j} keys: {childen.keys}")
+
+    print(tree.search(1))
+    print(tree.search(11))
     
-    
-tree = BPlusTree(max_degree=4)
-tree.insert(10)
-tree.insert(20)
-tree.insert(5)
-tree.insert(15)
-tree.insert(25)
-tree.insert(11)
-tree.insert(13)
-tree.insert(40)
-tree.insert(4)
-tree.insert(22)
-tree.insert(36)
-tree.insert(17)
-print(tree.search(11))
-
-
-print("Root keys:", tree.root.keys)
-# print("Child 1 keys:", tree.root.children[0].children[0].keys)
-# print("Child 2 keys:", tree.root.children[1].keys)
-# print("Child 2 keys:", tree.root.children[1].keys)
-
-for i, child in enumerate(tree.root.children):
-    print(f"child{i} keys: {child.keys}")
-    for j, childen in enumerate(child.children):
-        print(f"child{j} keys: {childen.keys}")
-
-
-
-
-
-
-# class TreeVisualizer(QGraphicsView):
-#     def __init__(self, tree):
-#         super().__init__()
-#         self.tree = tree
-#         self.scene = QGraphicsScene()
-#         self.setScene(self.scene)
-#         self.draw_tree()
-
-#     def draw_tree(self):
-#         self.scene.clear()
-#         self._draw_node(self.tree.root, 0, 400, 200)
-
-#     def _draw_node(self, node, depth, x, y):
-#         # Draw node
-#         node_item = QGraphicsEllipseItem(x - 30, y - 20, 70, 40)
-#         node_item.setBrush(QBrush(Qt.white))
-#         node_item.setPen(QPen(Qt.black))
-#         self.scene.addItem(node_item)
-
-#         # Draw keys inside node
-#         keys_text = QGraphicsTextItem(", ".join(map(str, node.keys)))
-#         keys_text.setPos(x - 25, y - 15)
-#         self.scene.addItem(keys_text)
-
-#         # Recursively draw children
-#         if not node.is_leaf:
-#             step = 50 * len(node.children)
-#             for i, child in enumerate(node.children):
-#                 child_x = x - step // 2 + i * 100
-#                 child_y = y + 150
-
-#                 # Draw line from parent to child
-#                 self.scene.addLine(x, y + 20, child_x, child_y - 20, QPen(Qt.black))
-
-#                 self._draw_node(child, depth + 1, child_x, child_y)
-
-# # Main code
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-
-#     # Build B+ tree
-#     tree = BPlusTree(max_degree=4)
-#     for key in [10, 20, 5, 15, 25, 11, 13, 40, 4, 22, 36, 17]:
-#         tree.insert(key)
-
-#     # Visualize the tree
-#     visualizer = TreeVisualizer(tree)
-#     visualizer.setWindowTitle("B+ Tree Visualization")
-#     visualizer.show()
-
-#     sys.exit(app.exec_())
